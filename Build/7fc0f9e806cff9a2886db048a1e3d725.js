4d4f3c7acb319f5822d3e854df9f42cbfb1027b0
David Fontes <dfontes@20miletech.com>
2013-05-09 14:47:55 -0400

    Post-live tweaks

--- Build/7fc0f9e806cff9a2886db048a1e3d725.js
+++
@@ -1,2 +0,0 @@
-(function(){$(function(){return $(document).foundation()}),$(".has-children").addClass("has-dropdown"),$(".has-children").children("ul").addClass("dropdown"),$(".top-bar-section li").after("<li class='divider'></li>"),$(".side-nav li").after("<li class='divider'></li>")}).call(this);
-;
